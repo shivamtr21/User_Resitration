35b7dc808f57fd35e1eea51ba8fc47b1db51ceb9
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-24 22:57:57 +0530

    [shivam] : Added user regitration problem 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Page!!");
+            Console.WriteLine("********************************");
+            Registration obj = new Registration();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(obj.ValidateFName(FName));
+        }
+    }
+}
---
+++ Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Registration
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
