d2125f724e32adf5b69292fdaf9962a078784be2
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-25 21:52:24 +0530

    [Shivam Tripathi] :Added User Registration Problem UC3

--- Registration.cs
+++ Registration.cs
@@ -10,17 +10,26 @@ namespace UserRegistration
     public class Registration
     {
         public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
-
         public static string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Email = "^[a-z0-9._+-]+@[a-z0-9.-]+\\.[a-z.]{2,}$";
+
+
+        //CHECK FIRST NAME
         public bool ValidateFName(string FName)
         {
             return Regex.IsMatch(FName, FirstName);
         }
 
+        //CHECK LAST NAME
         public bool ValidateLName(string LName)
         {
-            return Regex.IsMatch(LName, LastName);
+            return Regex.IsMatch(LName, FirstName);
+        }
 
+        //CHECK EMAIL
+        public bool ValidateEmail(string MailId)
+        {
+            return Regex.IsMatch(MailId, Email);
         }
     }
 
