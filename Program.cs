384ae2f6e2a3fd58420ae315d884c8cbfae213d7
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-25 21:52:59 +0530

    [Shivam Tripathi] :Added User Registration Problem UC3

--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@
         {
             Console.WriteLine("Welcome to User Registration Page!!");
             Console.WriteLine("********************************");
+
             Registration obj = new Registration();
             Console.WriteLine("Enter the First Name:");
             string FName = Console.ReadLine();
-            Console.WriteLine(obj.ValidateFName(FName));
-
-            Console.WriteLine("Enter the Last Name");
+            Console.WriteLine("Enter the Last Name:");
             string LName = Console.ReadLine();
-            Console.WriteLine(obj.ValidateLName(LName));
+            Console.WriteLine("Enter your Email Id:");
+            string MailId = Console.ReadLine();
+
+
+            Console.WriteLine(obj.ValidateFName(FName));
+            Console.WriteLine(obj.ValidateFName(LName));
+            Console.WriteLine(obj.ValidateEmail(MailId));
         }
     }
 }
